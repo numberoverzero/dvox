c9e334d179a64b3be867793f2b8a3311b95c7c69
Joe Cross <joe.mcross@gmail.com>
2015-08-09 02:08:35 -0700

    Add expiration to workers

--- dvox/engines.py
+++ dvox/engines.py
@@ -3,7 +3,8 @@ import boto3
 
 _PROFILE_NAME = "dvox-integ"
 _SESSION = boto3.session.Session(profile_name=_PROFILE_NAME)
-engine = bloop.Engine(session=_SESSION, atomic=False, consistent=True)
+update = engine = bloop.Engine(
+    session=_SESSION, atomic=False, consistent=False, save='update')
 model = engine.model
 overwrite = engine.context(save='overwrite')
 atomic = engine.context(atomic=True)
--- dvox/models/worker.py
+++ dvox/models/worker.py
@@ -1,24 +1,32 @@
+import arrow
 import uuid
-from bloop import Column, UUID, String, ConstraintViolation
+from bloop import Column, DateTime, UUID, String, ConstraintViolation
+from dvox import engines
 from dvox.app import config
-from dvox.engines import engine
 from dvox.exceptions import CreateFailed
 
 
-class Worker(engine.model):
+class Worker(engines.model):
     id = Column(UUID, hash_key=True)
     ip = Column(String)
+    expires = Column(DateTime, name='e')
 
     @classmethod
     def unique(cls, ip):
         retries = config["CREATE_RETRIES"]
-        does_not_exist = cls.id.is_(None)
         while retries:
-            obj = cls(id=uuid.uuid4(), ip=ip)
+            expires = arrow.now().replace(
+                seconds=config["WORKER_TIMEOUT_SECONDS"])
+            obj = cls(id=uuid.uuid4(), ip=ip, expires=expires)
             try:
-                engine.save(obj, condition=does_not_exist)
+                engines.atomic.save(obj)
             except ConstraintViolation:
                 retries -= 1
             else:
                 return obj
-        raise CreateFailed()
+        raise CreateFailed
+
+    def renew(self):
+        self.expires = arrow.now().replace(
+            seconds=config["WORKER_TIMEOUT_SECONDS"])
+        engines.update.save(self)
