d40b4dd0908bd9c5d45dc0897f26ad35ada61bd2
Joe Cross <joe.mcross@gmail.com>
2015-08-09 10:30:22 -0700

    Expose config through dvox base module

--- dvox/__init__.py
+++ dvox/__init__.py
@@ -1,4 +1,5 @@
+from dvox.app import config
 from dvox.engines import engine
 from dvox.models import Chunk, ChunkLock, Worker, World
 
-__all__ = ["engine", "Chunk", "ChunkLock", "Worker", "World"]
+__all__ = ["config", "engine", "Chunk", "ChunkLock", "Worker", "World"]
