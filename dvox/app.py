52add194c6eff5a1188d65db83acc9d951264b51
Joe Cross <joe.mcross@gmail.com>
2015-08-08 18:37:11 -0700

    Central config for dvox

---
+++ dvox/app.py
@@ -0,0 +1,12 @@
+import bloop
+import boto3.session
+
+_PROFILE_NAME = "dvox-integ"
+_SESSION = boto3.session.Session(profile_name=_PROFILE_NAME)
+engine = bloop.Engine(session=_SESSION)
+atomic_engine = engine.context(atomic=True)
+
+config = {
+    "CREATE_RETRIES": 5,
+    "CHUNK_LOCK_TIMEOUT_SECONDS": 30
+}
