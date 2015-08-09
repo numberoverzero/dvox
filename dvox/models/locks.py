cfd5967cca807e01b1d37389c631049c1cc5fd8b
Joe Cross <joe.mcross@gmail.com>
2015-08-09 10:30:54 -0700

    Massively simplify ChunkLock.acquire
    It's like I forgot how optimistic locking worked.  We can describe
    all the situations where it's valid to overwrite the lock, and
    then let DynamoDB do the validation.  Massive runtime
    improvement - at least 2x for expired locks, at least 3x for
    expired locks with contention.

--- dvox/models/locks.py
+++ dvox/models/locks.py
@@ -2,7 +2,7 @@ import arrow
 from bloop import Column, UUID, DateTime, ConstraintViolation, NotModified
 from dvox import engines
 from dvox.app import config
-from dvox.exceptions import InUse, Expired
+from dvox.exceptions import InUse
 from dvox.models.types import Position
 
 
@@ -18,40 +18,19 @@ class ChunkLock(engines.model):
         now = arrow.now()
         self.expires = now.replace(
             seconds=config["CHUNK_LOCK_TIMEOUT_SECONDS"])
+
+        can_overwrite = (
+            # Same worker, just renewing expiration
+            (ChunkLock.worker == self.worker) |
+            # Existing lock already expired
+            (ChunkLock.expires <= now) |
+            # No existing lock
+            ChunkLock.NOT_EXIST)
+
         try:
-            engines.overwrite.save(self, condition=ChunkLock.NOT_EXIST)
+            engines.overwrite.save(self, condition=can_overwrite)
         except ConstraintViolation:
-            existing_lock = self.current()
-
-            # Although there was a conflict with an existing lock,
-            # the next load found no lock.  Try again.
-            if existing_lock is None:
-                self.acquire()
-
-            # The last worker with the lock didn't check in before
-            # the timeout.  Try again.
-            elif now >= existing_lock.expires:
-                try:
-                    existing_lock.release()
-                # After the current lock was loaded, it was modified
-                # before we could release it.  Try again.
-                except InUse:
-                    pass
-                self.acquire()
-
-            # The existing lock was held by the same worker.  Try to renew.
-            elif existing_lock.worker == self.worker:
-                try:
-                    self.renew()
-                # After the current lock was loaded, it was modified (or
-                # deleted) before we could renew.  Try again.
-                except Expired:
-                    self.acquire()
-
-            # Existing lock is owned by a different worker and hasn't expired.
-            else:
-                raise InUse
-        # Success
+            raise InUse
 
     def release(self):
         """Release the lock on this chunk.
@@ -63,20 +42,7 @@ class ChunkLock(engines.model):
         except ConstraintViolation:
             raise InUse
 
-    def renew(self):
-        """Update the lock's expire time.
-
-        Raises `Expired` if the lock was deleted, or a different worker
-        owns the lock."""
-        self.expires = arrow.now().replace(
-            seconds=config["CHUNK_LOCK_TIMEOUT_SECONDS"])
-        try:
-            same_worker = ChunkLock.worker == self.worker
-            engines.overwrite.save(self, condition=same_worker)
-        except ConstraintViolation:
-            # The lock was deleted, or another worker took it over.
-            raise Expired
-
+    @property
     def current(self):
         """Return the current lock or None."""
         existing_lock = ChunkLock(world=self.world, chunk=self.chunk)
